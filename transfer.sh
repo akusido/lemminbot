fa7346112d1c9d0b4217561b8cc5618b5225957b
akusido <akusido@users.noreply.github.com>
2017-05-23 12:23:52 +0300

    Cronjob to upload finished files

---
+++ transfer.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+rsync -qaW --inplace --remove-source-files --exclude="*.iowait" /tmp/rsyncing/ /media/frames
+EILEN=$(date --date="-1 day" +"%Y%m%d")
+cd /media/frames/${EILEN}/
+tar cjf weather-${EILEN}.tar.bz2 weather/*.json
+rm -r weather/
+cd /media/frames/
+rsync -qaW --password-file /path/to/key --skip-compress=jpg 2017* rsync://user@destination/folder
+exit 0
+
+# Note: for best results, this script
+# should run nightly after 00:00 UTC!
